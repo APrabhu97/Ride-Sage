75bba2371a71f9aae7722c8f156d13468433c92c
ahmed <ahmed.iraq.98@gmail.com>
2021-11-10 16:41:50 -0600

    Booking Complete View

---
+++ app/src/main/java/com/anish/ridesage/BookingCompleteActivity.java
@@ -0,0 +1,14 @@
+package com.anish.ridesage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BookingCompleteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_booking_complete);
+    }
+}
