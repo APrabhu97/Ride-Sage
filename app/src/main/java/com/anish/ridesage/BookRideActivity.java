65882f4f85cd2d73c83b68b2bae8e8129e93a9a2
Sebek1232 <67287582+Sebek1232@users.noreply.github.com>
2021-11-10 19:59:50 -0600

    book ride activity

---
+++ app/src/main/java/com/anish/ridesage/BookRideActivity.java
@@ -0,0 +1,14 @@
+package com.anish.ridesage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BookRideActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.book_ride);
+    }
+}
