46fe3f7a6dc6aa91246e983c891d54d08d293def
Prabhu <aprabh29@uic.edu>
2021-11-26 12:13:10 -0600

    maps and directions added

--- app/src/main/java/com/anish/ridesage/BookRideActivity.java
+++ app/src/main/java/com/anish/ridesage/BookRideActivity.java
@@ -5,23 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.libraries.places.api.Places;
@@ -32,42 +38,86 @@ import com.google.android.libraries.places.api.net.PlacesClient;
 import java.util.Arrays;
 import java.util.List;
 
-public class BookRideActivity extends AppCompatActivity{
+public class BookRideActivity extends AppCompatActivity implements PointsParser.TaskLoadedCallback{
 
-    private GoogleMap mMap;
-    private CameraPosition cameraPosition;
-
-    // The entry point to the Places API.
     private PlacesClient placesClient;
+    private GoogleMap mMap;
+    private TextView source, destination;
+    private Place sourcePlace, destinationPlace;
+    private Polyline currentPolyline;
 
-    // The entry point to the Fused Location Provider.
-    private FusedLocationProviderClient fusedLocationProviderClient;
 
-    // A default location (Sydney, Australia) and default zoom to use when location permission is
-    // not granted.
-    private final LatLng defaultLocation = new LatLng(-33.8523341, 151.2106085);
-    private static final int DEFAULT_ZOOM = 15;
-    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
-    private boolean locationPermissionGranted;
-
-    // The geographical location where the device is currently located. That is, the last-known
-    // location retrieved by the Fused Location Provider.
-    private Location lastKnownLocation;
-
-    // Keys for storing activity state.
-    // [START maps_current_place_state_keys]
-    private static final String KEY_CAMERA_POSITION = "camera_position";
-    private static final String KEY_LOCATION = "location";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.book_ride);
+        source = findViewById(R.id.bookSource);
+        destination = findViewById(R.id.bookDestination);
+        Intent i = getIntent();
+        sourcePlace = i.getParcelableExtra("source");
+        destinationPlace = i.getParcelableExtra("destination");
+        source.setText(sourcePlace.getName());
+        destination.setText(destinationPlace.getName());
 
+        Places.initialize(this, getString(R.string.com_google_android_geo_API_KEY));
+        placesClient = Places.createClient(this);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync((map)-> {
+            this.mMap = map;
+            mMap.addMarker(new MarkerOptions()
+                    .position(sourcePlace.getLatLng()).title("Source"));
+            mMap.addMarker(new MarkerOptions()
+                    .position(destinationPlace.getLatLng()).title("Destination"));
+            updateLocationUI();
+            setRoute();
+        });
+    }
+
+    @SuppressLint("MissingPermission")
+    private void updateLocationUI() {
+        try {
+                mMap.setMyLocationEnabled(true);
+                mMap.getUiSettings().setMyLocationButtonEnabled(true);
+        } catch (SecurityException e)  {
+            Log.e("Exception: %s", e.getMessage());
+        }
     }
 
     public void onButtonClicked(View v){
         Intent myIntent = new Intent(this, BookingCompleteActivity.class);
         startActivity(myIntent);
+    }
+
+    private void setRoute(){
+        if(sourcePlace != null && destinationPlace != null){
+            String url = getUrl(sourcePlace.getLatLng(), destinationPlace.getLatLng(), "driving");
+            new FetchURL(this).execute(url, "driving");
+            LatLngBounds.Builder builder = new LatLngBounds.Builder();
+            builder.include(sourcePlace.getLatLng());
+            builder.include(destinationPlace.getLatLng());
+            int width = getResources().getDisplayMetrics().widthPixels;
+            int height = getResources().getDisplayMetrics().heightPixels;
+            int padding = (int) (width * 0.10);
+            CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(builder.build(), width, height, padding);
+            mMap.animateCamera(cu);
+        }
+    }
+    private String getUrl(LatLng origin, LatLng dest, String directionMode) {
+        String str_origin = "origin=" + origin.latitude + "," + origin.longitude;
+        String str_dest = "destination=" + dest.latitude + "," + dest.longitude;
+        String mode = "mode=" + directionMode;
+        String parameters = str_origin + "&" + str_dest + "&" + mode;
+        String url = "https://maps.googleapis.com/maps/api/directions/json?" + parameters + "&key=" + getString(R.string.com_google_android_geo_API_KEY);
+        return url;
+    }
+
+    @Override
+    public void onTaskDone(Object... values) {
+        if(currentPolyline != null){
+            currentPolyline.remove();
+        }
+        currentPolyline = mMap.addPolyline((PolylineOptions)values[0]);
     }
 
 }
--- app/src/main/java/com/anish/ridesage/FindRideActivity.java
+++ app/src/main/java/com/anish/ridesage/FindRideActivity.java
@@ -1,67 +1,63 @@
 package com.anish.ridesage;
 
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+import static android.Manifest.permission.ACCESS_WIFI_STATE;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
-
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AutoCompleteTextView;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.model.PlaceLikelihood;
+import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest;
 import com.google.android.libraries.places.api.net.PlacesClient;
 
-public class FindRideActivity extends AppCompatActivity {
+import java.util.Arrays;
+import java.util.List;
 
-    EditText source;
-    EditText destination;
+public class FindRideActivity extends AppCompatActivity  implements PointsParser.TaskLoadedCallback {
+
+    private static final String TAG = "FindRideActivity";
+    Place sourcePlace, destinationPlace;
+    AutoCompleteTextView sourceAutocomplete, destinationAutocomplete;
     private GoogleMap mMap;
-    private CameraPosition cameraPosition;
+    private Polyline currentPolyline;
 
-    // The entry point to the Places API.
     private PlacesClient placesClient;
-
-    // The entry point to the Fused Location Provider.
-    private FusedLocationProviderClient fusedLocationProviderClient;
-
-    // A default location (Sydney, Australia) and default zoom to use when location permission is
-    // not granted.
-    private final LatLng defaultLocation = new LatLng(-33.8523341, 151.2106085);
     private static final int DEFAULT_ZOOM = 15;
     private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
     private boolean locationPermissionGranted;
-
-    // The geographical location where the device is currently located. That is, the last-known
-    // location retrieved by the Fused Location Provider.
-    private Location lastKnownLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.find_ride);
-        source = (EditText) findViewById(R.id.findRideSource);
-        destination = (EditText) findViewById(R.id.findRideDestination);
+        sourceAutocomplete = (AutoCompleteTextView) findViewById(R.id.findRideSource);
+        destinationAutocomplete = (AutoCompleteTextView) findViewById(R.id.findRideDestination);
 
-                Places.initialize(getApplicationContext(), getString(R.string.com_google_android_geo_API_KEY));
+        Places.initialize(this, getString(R.string.com_google_android_geo_API_KEY));
         placesClient = Places.createClient(this);
-
-        // Construct a FusedLocationProviderClient.
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-
+        initAutocomplete();
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync((map)-> {
@@ -70,40 +66,58 @@ public class FindRideActivity extends AppCompatActivity {
             updateLocationUI();
             getDeviceLocation();
         });
+
+    }
+
+    private void initAutocomplete(){
+        Log.d(TAG, "init: initializing");
+        PlaceAutocompleteAdapter sourceAdapter = new PlaceAutocompleteAdapter(this, placesClient, (place) -> {
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(place.getLatLng(), DEFAULT_ZOOM));
+            mMap.addMarker(new MarkerOptions()
+                    .position(place.getLatLng()).title("Source"));
+            sourcePlace = place;
+            sourceAutocomplete.setText(place.getName());
+            setRoute();
+            sourceAutocomplete.dismissDropDown();
+            hideKeyboard();
+        });
+        sourceAutocomplete.setAdapter(sourceAdapter);
+
+        PlaceAutocompleteAdapter destinationAdapter = new PlaceAutocompleteAdapter(this, placesClient, (place) -> {
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(place.getLatLng(), DEFAULT_ZOOM));
+            mMap.addMarker(new MarkerOptions()
+                    .position(place.getLatLng()).title("Destination"));
+            destinationPlace = place;
+            destinationAutocomplete.setText(place.getName());
+            setRoute();
+            destinationAutocomplete.dismissDropDown();
+            hideKeyboard();
+        });
+        destinationAutocomplete.setAdapter(destinationAdapter);
     }
 
     public void onButtonClicked(View v){
         Intent myIntent = new Intent(this, CabListings.class);
-        myIntent.putExtra("source", source.getText().toString());
-        myIntent.putExtra("destination", destination.getText().toString());
+        myIntent.putExtra("source", sourcePlace);
+        myIntent.putExtra("destination", destinationPlace);
         startActivity(myIntent);
     }
 
 
+    @SuppressLint("MissingPermission")
     private void getDeviceLocation() {
         try {
             if (locationPermissionGranted) {
-                Task<Location> locationResult = fusedLocationProviderClient.getLastLocation();
-                locationResult.addOnCompleteListener(this, new OnCompleteListener<Location>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Location> task) {
-                        if (task.isSuccessful()) {
-                            // Set the map's camera position to the current location of the device.
-                            lastKnownLocation = task.getResult();
-                            if (lastKnownLocation != null) {
-                                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
-                                        new LatLng(lastKnownLocation.getLatitude(),
-                                                lastKnownLocation.getLongitude()), DEFAULT_ZOOM));
-                            }
-                        } else {
-                            Log.d("BookRideActivity", "Current location is null. Using defaults.");
-                            Log.e("BookRideActivity", "Exception: %s", task.getException());
-                            mMap.moveCamera(CameraUpdateFactory
-                                    .newLatLngZoom(defaultLocation, DEFAULT_ZOOM));
-                            mMap.getUiSettings().setMyLocationButtonEnabled(false);
-                        }
-                    }
-                });
+                if (ContextCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, ACCESS_WIFI_STATE) == PackageManager.PERMISSION_GRANTED){
+                    List<Place.Field> placeFields = Arrays.asList(Place.Field.NAME, Place.Field.LAT_LNG, Place.Field.ID);
+                    FindCurrentPlaceRequest request =
+                            FindCurrentPlaceRequest.builder(placeFields).build();
+                    placesClient.findCurrentPlace(request).addOnSuccessListener((response) -> {
+                        List<PlaceLikelihood> placeLikelihoods = response.getPlaceLikelihoods();
+                        Place place = placeLikelihoods.get(0).getPlace();
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(place.getLatLng(), DEFAULT_ZOOM));
+                    });
+                }
             }
         } catch (SecurityException e)  {
             Log.e("Exception: %s", e.getMessage(), e);
@@ -112,12 +126,12 @@ public class FindRideActivity extends AppCompatActivity {
 
     private void getLocationPermission() {
         if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
-                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
             locationPermissionGranted = true;
         } else {
             ActivityCompat.requestPermissions(this,
-                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    new String[]{ACCESS_FINE_LOCATION},
                     PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
         }
     }
@@ -140,6 +154,7 @@ public class FindRideActivity extends AppCompatActivity {
         updateLocationUI();
     }
 
+    @SuppressLint("MissingPermission")
     private void updateLocationUI() {
         if (mMap == null) {
             return;
@@ -151,7 +166,6 @@ public class FindRideActivity extends AppCompatActivity {
             } else {
                 mMap.setMyLocationEnabled(false);
                 mMap.getUiSettings().setMyLocationButtonEnabled(false);
-                lastKnownLocation = null;
                 getLocationPermission();
             }
         } catch (SecurityException e)  {
@@ -159,4 +173,39 @@ public class FindRideActivity extends AppCompatActivity {
         }
     }
 
+    private void setRoute(){
+        if(sourcePlace != null && destinationPlace != null){
+            String url = getUrl(sourcePlace.getLatLng(), destinationPlace.getLatLng(), "driving");
+            new FetchURL(this).execute(url, "driving");
+            LatLngBounds.Builder builder = new LatLngBounds.Builder();
+            builder.include(sourcePlace.getLatLng());
+            builder.include(destinationPlace.getLatLng());
+            int width = getResources().getDisplayMetrics().widthPixels;
+            int height = getResources().getDisplayMetrics().heightPixels;
+            int padding = (int) (width * 0.10);
+            CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(builder.build(), width, height, padding);
+            mMap.animateCamera(cu);
+        }
+    }
+    private String getUrl(LatLng origin, LatLng dest, String directionMode) {
+        String str_origin = "origin=" + origin.latitude + "," + origin.longitude;
+        String str_dest = "destination=" + dest.latitude + "," + dest.longitude;
+        String mode = "mode=" + directionMode;
+        String parameters = str_origin + "&" + str_dest + "&" + mode;
+        String url = "https://maps.googleapis.com/maps/api/directions/json?" + parameters + "&key=" + getString(R.string.com_google_android_geo_API_KEY);
+        return url;
+    }
+
+    @Override
+    public void onTaskDone(Object... values) {
+        if(currentPolyline != null){
+            currentPolyline.remove();
+        }
+        currentPolyline = mMap.addPolyline((PolylineOptions)values[0]);
+    }
+
+    private void hideKeyboard(){
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(findViewById(android.R.id.content).getRootView().getWindowToken(), 0);
+    }
 }
